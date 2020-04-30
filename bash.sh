0c9799c2954ee85746fcff022b13336d26744a9a
Magnus Kokk <magnus@kokk.eu>
2020-04-30 20:12:28 +0300

    Add rustup volume

--- bash.sh
+++ bash.sh
@@ -8,6 +8,7 @@ createVolume() {
 
 createVolume "dev_env_go"
 createVolume "dev_env_nvim"
+createVolume "dev_env_rustup"
 
 mkdir -p ./code
 
@@ -16,6 +17,7 @@ docker run \
 	-h dev_env \
 	--mount source=dev_env_go,target=/home/$USER/go \
 	--mount source=dev_env_nvim,target=/home/$USER/.config/nvim \
+	--mount source=dev_env_rustup,target=/home/$USER/.rustup \
 	--mount type=bind,source=$(pwd)/code,target=/code \
 	--mount type=bind,source=/home/$USER/.ssh,target=/home/$USER/.ssh \
 	--rm \
