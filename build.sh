1f43aa4540a4e09e25c348a0ec65d1b7057fc3f8
Magnus Kokk <magnus@kokk.eu>
2020-05-01 18:20:11 +0300

    Wrap gopls with autorestart

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 
 docker-compose build \
 	--build-arg user=$USER \
-	--build-arg dot_commit=8bca1ebcb9da2ff19ea6d4cde8434b37a1bd6830 \
-	dev-env
+	--build-arg dot_commit=6f40358 \
+	--force-rm \
+	"$@"
 
