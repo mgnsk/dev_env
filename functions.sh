d700af65f590ca1cce40ed8548ef379fd762e635
Magnus Kokk <magnus@kokk.eu>
2020-05-01 12:05:13 +0300

    Update dotfiles, multistage build

--- functions.sh
+++ functions.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-createVolume() {
-	if ! docker volume ls | awk 'NR!=1 { print $2 }' | grep -q $1; then
-		docker volume create $1
-	fi
-}
+#createVolume() {
+# 	if ! docker volume ls | awk 'NR!=1 { print $2 }' | grep -q $1; then
+#		docker volume create $1
+#	fi
+#}
 
 run() {
 	docker run \
