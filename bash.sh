c5a38fcd500f3e0d55561957114131daddbaef3a
Magnus Kokk <magnus@kokk.eu>
2020-05-01 14:48:51 +0300

    Simplify running

--- bash.sh
+++ bash.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-run() {
-	docker run \
-		-it \
-		-m 2G \
-		-h $1 \
-		--mount type=bind,source=$(pwd)/code,target=/code \
-		--mount type=bind,source=/home/$(id -un)/.ssh,target=/home/$(id -un)/.ssh \
-		--rm \
-		"$@"
-}
-
 mkdir -p ./code
 
-run $1 /bin/bash
+docker run \
+	-it \
+	--memory-swappiness=0 \
+	-h "$1" \
+	--mount type=bind,source=$(pwd)/code,target=/code \
+	--mount type=bind,source=/home/$(id -un)/.ssh,target=/home/$(id -un)/.ssh \
+	--rm \
+	-m 2G \
+	"$1" \
+	/bin/bash
 
