dc45661237ea3635f18c92ba53e5ce072ebe9f07
Magnus Kokk <magnus@kokk.eu>
2020-05-03 15:06:13 +0300

    Update dotfiles, remove goproxy

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@
 docker-compose build \
 	--build-arg user=$(id -un) \
 	--build-arg dot_url=https://github.com/mgnsk/dotfiles.git \
-	--build-arg dot_commit=a8c86a277836948f61b59a1c39214f63319ec378
+	--build-arg dot_commit=45465bece491e854171c09ae70a8a73ca0123909
 
