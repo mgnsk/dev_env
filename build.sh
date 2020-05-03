7b571fdfc5d2cd0584fc6003a8bf972b27df9c71
Magnus Kokk <magnus@kokk.eu>
2020-05-04 01:07:32 +0300

    Update

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 docker-compose build \
-	--build-arg user=$(id -un) \
+	--build-arg user=$USER \
 	--build-arg dot_url=https://github.com/mgnsk/dotfiles.git \
-	--build-arg dot_commit=65a7d62f9f21484352a1bfa8c2d683754d18646f
+	--build-arg dot_commit=61a1c434a2a1b8907417dfa0c833886a9c11f7fe
 
