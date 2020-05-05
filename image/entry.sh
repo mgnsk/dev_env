c88b6f06b067825ab31a8a442bf791cf88696883
Magnus Kokk <magnus@kokk.eu>
2020-05-05 22:19:00 +0300

    Backup

--- image/entry.sh
+++ image/entry.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+set -euo pipefail
+
 earlyoom &>/dev/null &
+
+useradd -m -G wheel -s /bin/bash ${user}
+
+echo "%wheel ALL=(ALL) ALL" >> /etc/sudoers
+echo "Defaults:${user} !authenticate" >> /etc/sudoers 
 
 sh -c "$@"
 
