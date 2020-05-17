3ad75fa7f08dbdee99dafdcbaf0acfed1504b2bc
Magnus Kokk <magnus@kokk.eu>
2020-05-17 03:48:02 +0300

    add direnv permission

--- image/install_wheel.sh
+++ image/install_wheel.sh
@@ -7,6 +7,7 @@ set -euo pipefail
 declare -a dirs=(
 	"/homedir/.config/coc"
 	"/homedir/.local"
+	"/homedir/.local/share/direnv"
 	"/homedir/.cache"
 	"/homedir/.vim/plugged/gopher.vim/tools/bin"
 )
